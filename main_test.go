83346f6d6277d940a31cc0136d88c22d008b3f59
Aditya Natraj <adityajones@gmail.com>
2021-01-07 13:08:52 -0500

    add test for wrong modifier

--- main_test.go
+++ main_test.go
@@ -219,6 +219,15 @@ func TestParseAndRunConfig(t *testing.T) {
 			planExpectedOk:       false,
 			cfgExpectedProjectId: "my-project-id",
 		},
+		{
+			env: map[string]string{
+				"PLUGIN_ACTION": "deploy", "PLUGIN_SERVICE": "my-service",
+				"PLUGIN_IMAGE": "my-image", "PLUGIN_TOKEN": validGCPKey,
+				"PLUGIN_CLOUD_SQL_UPDATE": "&wrong-modifier"},
+			cfgExpectedOk:        true,
+			planExpectedOk:       false,
+			cfgExpectedProjectId: "my-project-id",
+		},
 	} {
 		name := fmt.Sprintf("env:[%s]", tst.env)
 		t.Run(name, func(t *testing.T) {
